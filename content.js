e817efc7cba7d8f48e55d078b0bf7d0c9003c310
johannarejoicemvula10-hue <johannarejoicemvula10@gmail.com>
2026-03-02 23:04:58 +0200

    Add content file for editable text

---
+++ content.js
@@ -0,0 +1,29 @@
+// content.js (ONLY text lives here)
+
+window.ANNIVERSARY_CONTENT = {
+  reasonsTitle: "10 Reasons I Still Choose You",
+  reasons: [
+    "Your kindness makes me feel safe.",
+    "Your consistency is my peace.",
+    "You loved me through war and never left.",
+    "You lead with patience and strength.",
+    "You protect what we have.",
+    "You make love feel simple and sure.",
+    "You listen with your heart.",
+    "You pray for me and with me.",
+    "You make me want to be better.",
+    "You feel like home."
+  ],
+
+  letterTitle: "My Letter To You",
+  letterHtml: `
+    <p>
+      Cliff,<br><br>
+      Four years… and I still don’t have perfect words, but I know this:
+      I choose you with intention. I choose you with peace. I choose you with prayer.<br><br>
+      (Write your full letter here...)<br><br>
+      Love,<br>
+      Jojo
+    </p>
+  `
+};
